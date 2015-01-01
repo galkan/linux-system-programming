0dbd572147c161acbb98f59a064854186d609dba
Gökhan ALKAN <cigalkan@gmail.com>
2015-01-01 12:22:53 +0200

    Create dosya_betimleyici_tablo_uzunlugu.c

---
+++ dosya_betimleyici_tablo_uzunlugu.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void exitsys(const char *msg);
+int main(int argc, char *argv[])
+{
+     int i;
+     int fd;
+
+     for (i = 1;; ++i) {
+          if ((fd = open("sample.c", O_RDONLY)) < 0)  
+               break;
+          printf("count = %d, fd = %d\n", i, fd);
+     }
+
+     return 0;
+}
+
+void exitsys(const char *msg)
+{
+     perror(msg);
+     exit(EXIT_FAILURE);
+}
