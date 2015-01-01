ab9fb743ea9ea18f8ab6beb904f45119fef236d2
Gökhan ALKAN <cigalkan@gmail.com>
2015-01-01 12:43:27 +0200

    Create execv3_fonksiyonu_kullanimi.c

---
+++ execv3_fonksiyonu_kullanimi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+void exitsys(const char *msg);
+
+int main(int argc, char *argv[])
+{
+ if (argc < 2) {
+  fprintf(stderr, "wrong number of arguments!..\n");
+  exit(EXIT_FAILURE);
+ }
+
+ if (execp(argv[1], &argv[1]) < 0)
+  exitsys("execl");
+
+ printf("unreachable code!..\n");
+
+ return 0;
+}
+
+void exitsys(const char *msg)
+{
+ perror(msg);
+ exit(EXIT_FAILURE);
+}
